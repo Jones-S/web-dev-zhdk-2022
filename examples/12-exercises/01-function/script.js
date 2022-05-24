9c858e718cf2302a7bee6621decb9ff7edbb01cc
Jonas <git@jonasscheiwiller.ch>
2022-05-24 11:28:17 +0200

    feat: add starter kit for exercises

---
+++ examples/12-exercises/01-function/script.js
@@ -0,0 +1,7 @@
+console.info("Everything looks fine ✅.");
+console.log("Open this script.js file and write your code...");
+
+// write a function that multiplies three numbers
+// that could be passed into the function as parameters (inputs)
+
+// execute the function and log the result to the console.
---
+++ examples/12-exercises/02-class-change/script.js
@@ -0,0 +1,4 @@
+console.info("Everything looks fine ✅.");
+console.log("Open this script.js file and write your code...");
+
+// Select the div (blue box) in the index html and change its looks.
