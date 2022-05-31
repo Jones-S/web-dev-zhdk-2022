d2116f3450b8e2ea52b38d92c76a5dba5e2ae289
Jonas <git@jonasscheiwiller.ch>
2022-05-31 15:44:43 +0200

    feat(exercises): add solutions found in class

--- examples/12-exercises/01-function/script.js
+++ examples/12-exercises/01-function/script.js
@@ -5,3 +5,29 @@ console.log("Open this script.js file and write your code...");
 // that could be passed into the function as parameters (inputs)
 
 // execute the function and log the result to the console.
+
+// const number1 = 3;
+// const number2 = 1;
+// const number3 = 2;
+
+// console.log(number1 * number2 * number3);
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+const result = multiply(1, 2, 4);
+
+console.log("result: ", result);
+
+function multiplyWithObject(numbers) {
+  return numbers.a * numbers.b * numbers.c;
+}
+
+const myNumbers = {
+  c: 10,
+  a: 1,
+  b: 2,
+  otherkey: "name",
+};
+multiplyWithObject(myNumbers);
